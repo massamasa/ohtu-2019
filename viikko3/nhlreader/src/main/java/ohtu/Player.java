7a82876447b66e5bb0a81020f3592b5475d119d4
Matti Österbacka <matti.osterbacka@gmail.com>
2019-11-14 16:02:39 +0200

    tehtävä 1

---
+++ viikko3/nhlreader/src/main/java/ohtu/Player.java
@@ -0,0 +1,74 @@
+
+package ohtu;
+
+public class Player {
+    private int assists;
+    private int goals;
+    private String name;
+    private int penalties;
+    private String team;
+    private String nationality;
+    private String birthdate;
+
+    public int getAssists() {
+        return assists;
+    }
+
+    public void setAssists(int assists) {
+        this.assists = assists;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public void setGoals(int goals) {
+        this.goals = goals;
+    }
+
+    public int getPenalties() {
+        return penalties;
+    }
+
+    public void setPenalties(int penalties) {
+        this.penalties = penalties;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        this.team = team;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - Born: " + birthdate;
+    }
+      
+}
